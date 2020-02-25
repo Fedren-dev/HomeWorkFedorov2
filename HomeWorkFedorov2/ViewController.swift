59aec06ed98f35b6347dec83f402d6c65b178194
Alexander <fedrennn@gmail.com>
2020-02-25 19:34:37 +0200

    Initial Commit

---
+++ HomeWorkFedorov2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkFedorov2
+//
+//  Created by Александр Федоров on 25.02.2020.
+//  Copyright © 2020 Alexander Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
